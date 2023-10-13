6618c904cc33c9a816b30e4a10e770bb99d4caa9
Dolunay <pazarcidolunay@gmail.com>
2023-10-13 23:50:13 +0300

    file location changes

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -6,7 +6,7 @@ const Login = () => {
 
     return (
         <div className='bg-black flex flex-col items-center justify-center w-full h-screen'>
-            <img className='w-[400px] mb-12' src="./assets/spotify-logo.png" alt="" />
+            <img className='w-[400px] mb-12' src="https://storage.googleapis.com/pr-newsroom-wp/1/2018/11/Spotify_Logo_CMYK_White.png" alt="" />
             <a href={loginUrl} className='rounded-lg p-4 bg-green-500 text-white'>Login With Spotify</a>
         </div>
     )
