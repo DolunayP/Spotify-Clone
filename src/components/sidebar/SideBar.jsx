de6acd1ef1c3aff815fa4a332e342989854b7a9f
Dolunay <pazarcidolunay@gmail.com>
2023-10-13 23:43:46 +0300

    file location changes

--- src/components/sidebar/SideBar.jsx
+++ src/components/sidebar/SideBar.jsx
@@ -6,7 +6,7 @@ const SideBar = ({ word, setWord }) => {
     return (
         <div className='flex-[0.2] px-4 h-screen border-1 min-w-[220px] bg-black text-white '>
             <div className=' mt-4 w-[75px] m-auto cursor-pointer'>
-                <img className="w-full h-full" src="../src/assets/music-note.png" alt="" />
+                <img className="w-full h-full" src="./assets/music-note.png" alt="" />
             </div>
 
             <SidebarOptions word={word} setWord={setWord} />
